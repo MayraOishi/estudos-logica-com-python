346000fce8206a0a24a9a958375c8e4e6f4b6ebf
Mayra Oishi <namie.oishi@gmail.com>
2025-02-16 18:08:38 -0300

    Atualizando

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,10 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+# Variável que armazena uma string
+mensagem = "Olá, mundo!"
+
+# Lista que armazena várias strings
+lista_de_mensagens = ["Olá!", "Bem-vindo ao código!", "Aprendendo Python!", "DevOps é incrível!"]
+
+# Exibindo os valores
+print(mensagem)
+print(lista_de_mensagens)
